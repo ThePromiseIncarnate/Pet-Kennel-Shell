35242debf798e847dbfa852286954dce0676b60b
ThePromiseIncarnate <ThePromiseIncarnate@gmail.com>
2024-08-12 20:51:49 -0500

    My Cat

---
+++ index.js
@@ -0,0 +1,16 @@
+const pet = {
+    name: "Sonya",
+    species: "Cat",
+    happiness: "50",
+    play: function() {
+        this.happiness += 10;
+        console.log(`You played with ${this.name}! Happiness is now ${this.happiness}.`)
+    },
+    feed: function() {
+        this.happiness += 20;
+        console.log(`You fed ${this.name}! Happiness is now ${this.happiness}.`)
+    }, 
+    status: function() {
+        console.log(`Pet Name: ${this.name}, Species: ${this.species}, Happiness: ${this.happiness}`)
+    },
+}
